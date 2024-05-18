25e84a8262c8d9fb60ff119bddb02bdb50846c36
Everton Farias <evertondefarias115@gmail.com>
2024-05-18 13:56:34 -0300

    feat: Terminei o Crud de Produtos

--- inventory/src/main/java/com/example/inventory/controllers/ProductController.java
+++ inventory/src/main/java/com/example/inventory/controllers/ProductController.java
@@ -8,12 +8,14 @@ import com.example.inventory.repositories.ProductRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDateTime;
@@ -25,6 +27,10 @@ import java.util.UUID;
 public class ProductController {
     @Autowired
     ProductRepository productRepository;
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter();
+    }
 
 
     @PostMapping("/products")
@@ -40,6 +46,7 @@ public class ProductController {
 
         var productModel = new ProductModel();
         BeanUtils.copyProperties(productRecordDto, productModel);
+        productModel.setCategory(productRecordDto.getCategoryId()) ;
         System.out.println("COPIADO COM SUCESSO\n");
         System.out.println(productModel.toString());
         // Define a data de criação como a data e hora atuais
@@ -49,7 +56,7 @@ public class ProductController {
 
         return "redirect:/products";
     }
-    @PostMapping("/products/{id}")
+    @PutMapping("/products/{id}")
     public String updateProduct(@PathVariable("id") long id, @Valid ProductRecordDto productRecordDto, BindingResult result, RedirectAttributes redirectAttributes) {
 
 
@@ -63,6 +70,23 @@ public class ProductController {
         System.out.println(productModel.toString());
         return "redirect:/products"; // Redireciona para a página de listagem de produtos
 
+    }
+
+    @DeleteMapping("/products/{id}")
+    public String deleteProduct(@PathVariable("id") long id) {
+        // Verifica se o produto existe antes de tentar deletá-lo
+        Optional<ProductModel> productOptional = productRepository.findById(id);
+        if (!productOptional.isPresent()) {
+            // Produto não encontrado, redirecionar ou retornar erro
+            return "redirect:/products"; // Ou outra página de sua escolha
+        }
+
+        // Produto encontrado, proceda com a exclusão
+        ProductModel product = productOptional.get();
+        productRepository.delete(product);
+
+        // Redireciona para a página de lista de produtos após a exclusão
+        return "redirect:/products";
     }
 
 
